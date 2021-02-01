74415dd4572958d82126209d08574f7cefbc572d
dape79 <76449316+dape79@users.noreply.github.com>
2021-02-01 21:45:05 +0000

    Deploying to gh-pages from master @ dabd3bc15cebde61db7bf4592c507edad255b421 🚀

---
+++ KA.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="KA" tilewidth="32" tileheight="32" tilecount="5" columns="5">
+ <image source="KA-Pfeil.png" width="189" height="63"/>
+</tileset>
